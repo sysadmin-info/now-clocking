912d8d177c9af6704375393ee8f405cf313b82f9
Adrian <adrian.ambroziak@gmail.com>
2020-05-26 22:17:04 +0200

    Modified files to adapt them to modern conky configuration

--- conky-delay.sh
+++ conky-delay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-conky -c ~/.conky/now-playing/np.conkyrc & sleep 1s
-conky -c ~/.conky/now-playing/npart.conkyrc & sleep 1s
+conky -c ~/.config/conky/np.conkyrc & sleep 1s
+conky -c ~/.config/conky/npart.conkyrc & sleep 1s
 exit
